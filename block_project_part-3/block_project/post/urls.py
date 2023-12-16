c4dc426de181c2e0629cb83394f3c7eb9aeb68e4
Tipu Sultan <sultanmdtipu1996@gmail.com>
2023-12-16 22:00:07 +0800

    block_project_part-3_codes

---
+++ block_project_part-3/block_project/post/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .import views
+
+
+urlpatterns = [
+    # path('add/', views.add_post, name='add_post'),
+    path('add/', views.addPostCreateView.as_view(), name='add_post'),
+    # path('edit/<int:id>', views.edit_post, name='edit_post'),
+    path('edit/<int:id>/', views.editPostView.as_view(), name='edit_post'),
+    # path('delete/<int:id>', views.delete_post, name='delete_post'),
+    path('delete/<int:id>/', views.deleteView.as_view(), name='delete_post'),
+    path('details/<int:id>/', views.viewPostDetails.as_view(), name='detail_post'),
+]
+
